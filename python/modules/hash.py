8e93166b030fbbc9c2649d52403f259d73bed072
HizkiFW <hizkifw@gmail.com>
2016-09-11 16:14:05 +0700

    Added comments for easier module integration

--- python/modules/hash.py
+++ python/modules/hash.py
@@ -1,5 +1,8 @@
-import sys
-import hashlib
+#CCModule: Hash
+#Description: Returns the hash of a given string
+#Arguments: algorithm:string, data:string
+
+import sys, hashlib
 
 if len(sys.argv) > 2:
 	h = hashlib.new(sys.argv[1])
@@ -7,5 +10,5 @@ if len(sys.argv) > 2:
 	print(h.hexdigest())
 else:
 	print("Usage: hash.py algo data\n")
-	print("Supported algos: ")
+	print("Supported algos:")
 	print(" ".join(hashlib.algorithms_available))
