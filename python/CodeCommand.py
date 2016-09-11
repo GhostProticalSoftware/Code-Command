04f9959d325a183c15027b3079894510d99bf873
HizkiFW <hizkifw@gmail.com>
2016-09-11 16:25:08 +0700

    Added basic modular functionality

--- python/CodeCommand.py
+++ python/CodeCommand.py
@@ -1,14 +1,27 @@
-import sys
-import os
+import sys, os, glob
 
-def welcome():
-	print("CodeCommand CLI")
+modules = []
+mnames = []
+
+def init():
+	print("Scanning for modules")
+	for file in os.listdir("./modules"):
+		if file.endswith(".py"):
+			modules.append(file)
+			with open("./modules/" + file, "r") as f:
+				n = f.readline()[10:].strip()
+				mnames.append(n)
+				print("    " + n)
 	
+	print("\n" + str(len(modules)) + " found\n")
 	
+init()
+
 if len(sys.argv) > 1:
 	i = sys.argv[1]
 else:
-	welcome()
+	#welcome()
+	print("Enter module name for more options")
 	i = raw_input("> ")
 
 print(i)
